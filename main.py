19eb59c341f81d88cdc32cb5782e7e40eebdea59
Victor <muryn.pn@ucu.edu.ua>
2022-10-19 21:17:41 +0300

    4nd task is done

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 def reflexive_relation(matrix: list[list[int]]) -> list[list[int]]:
     """
     Finds reflexive relation of matrix.
-    To be simple, changes main diagonal to 1
+    To be simple, changes main diagonal to 1.
+    Returns matrix.
 
     >>> reflexive_relation([
     ...     [0, 0, 0, 1, 0],
@@ -25,6 +26,7 @@ def reflexive_relation(matrix: list[list[int]]) -> list[list[int]]:
 def symmetrical_relation(matrix: list[list[int]]) -> list[list[int]]:
     """
     Finds symmetrical relation. For every pair (a, b) must be (b, a) in hiccup.
+    Returns matrix.
 
     >>> symmetrical_relation([
     ...     [0, 0, 0, 1, 0],
@@ -49,3 +51,45 @@ def symmetrical_relation(matrix: list[list[int]]) -> list[list[int]]:
                 matrix[j][i] = 1
 
     return matrix
+
+
+def search_equivalence_classes(matrix: list[list[int]]) -> list[list[int]]:
+    """
+    Finds equivalence classes of matrix. Matrix must be symmetrical, reflexive and reflective.
+    Returns list of classes. Classes are lists of ints.
+
+    >>> search_equivalence_classes([
+    ... [1, 1, 0, 0],
+    ... [1, 1, 0, 0],
+    ... [0, 0, 1, 0],
+    ... [0, 0, 0, 1]
+    ... ])
+    [[1, 2], [3], [4]]
+
+    >>> search_equivalence_classes([
+    ... [1, 0, 1, 1],
+    ... [0, 1, 0, 0],
+    ... [1, 0, 1, 1],
+    ... [1, 0, 1, 1]
+    ... ])
+    [[1, 3, 4], [2]]
+    """
+    classes = [set() for _ in range(len(matrix))]
+
+    for i, row in enumerate(matrix):
+        for j, col in enumerate(row):
+            if col == 1:
+                classes[i].add(j + 1)
+                classes[j].add(i + 1)
+
+
+    # delete repeating classes
+    unique_classes = []
+    for i in classes:
+        list_class = list(i)
+
+        if list_class not in unique_classes:
+            unique_classes.append(list_class)
+
+
+    return unique_classes
